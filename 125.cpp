023cbf895b74b80ceed08e20542fbaf631c4743c
KQL <shutong.luo@gmail.com>
2019-12-30 10:20:51 -0800

    125. Valid Palindrome

--- 125.cpp
+++ 125.cpp
@@ -1,1 +1,24 @@
+// Date: 12/30/2019
+// Author: Tong Xu
 
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        if(s.empty()) return true;
+        int l = 0, r = s.size()-1;
+        while(l <= r) {
+            if(!isalnum(s[l])) {
+                l++;
+                continue;
+            }
+            if(!isalnum(s[r])) {
+                r--;
+                continue;
+            }
+
+            if(toupper(s[l]) != toupper(s[r])) return false;
+            l++, r--;
+        }
+        return true;
+    }
+};
