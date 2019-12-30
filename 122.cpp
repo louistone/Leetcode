71b0b6a05a2beae764d9f0256385ab5cf3425f3f
Tong Xu <shutong.luo@gmail.com>
2019-12-30 15:36:36 -0800

    solution for 122

--- 122.cpp
+++ 122.cpp
@@ -1,2 +1,14 @@
 // Author: Tong Xu
 // Date: 12/30/2019
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+      if(prices.empty()) return 0;
+      int maxProfit = 0, n = prices.size();
+      for(int  i = 0; i < n-1; i++) {
+        maxProfit += prices[i+1] > prices[i] ? prices[i+1]-prices[i] : 0;
+      }
+      return maxProfit;
+    }
+};
