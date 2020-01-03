c7735c3ce60bfef04fddf3f4e5601a4fcf078cfc
Tong Xu <tongxu@Tongs-MacBook-Pro.local>
2020-01-02 18:37:58 -0800

    2 pointer + greedy for 42. One catch move either l pointer or r pointer at one time

---
+++ 42.cpp
@@ -0,0 +1,23 @@
+//Author: Tong Xu
+//Date: 1/2/2020
+
+// 42: Trapping Rain Water this problem 11: Container with Most Water
+
+class Solution {
+public:
+  int trap(vector<int>& height) {
+    if(height.empty()) return 0;
+    int l = 0, r = height.size()-1, maxLeft = 0, maxRight = 0, res = 0, h;
+
+    while(l < r) {
+      maxLeft = max(maxLeft, height[l]);
+      maxRight = max(maxRight, height[r]);
+      h = min(maxLeft, maxRight);
+      res += h >= height[l] ? h - height[l] : 0;
+      res += h >= height[r] ? h - height[r] : 0;
+      l++, r--;
+    }
+
+    return res;
+  }
+}
